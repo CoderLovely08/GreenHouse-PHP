ebf543f9d15487be1d1f5def8330ebee38ad3905
CoderLovely08 <lovelysingeras297@gmail.com>
2022-09-10 17:27:19 +0530

    User login feature enrolled

--- Task1/process.php
+++ Task1/process.php
@@ -4,7 +4,7 @@ session_start();
 include 'config.php';
 $connectionString = "host=" . $config['DB_HOST'] . " port =5432 dbname=" . $config['DB_DATABASE'] . " user=" . $config['DB_USERNAME'] . " password=" . $config['DB_PASSWORD'];
 $conn = pg_connect($connectionString);
-$myfile = fopen("testfile.txt", "w");
+$myfile = fopen("logfile.txt", "w");
 
 if (!$conn) {
     echo 'something went wrong!';
@@ -12,13 +12,15 @@ if (!$conn) {
 }
 
 if (isset($_POST['action'])) {
+    fwrite($myfile,'inside action \n');
     if ($_POST['action'] == 'register'){
         register();
-    }else if($_POST['action'] == 'login') login();
-    // elseif ($_POST['action'] == 'login') login();
+    }else if($_POST['action'] == 'login'){
+        fwrite($myfile,'login called');
+        login();
+    }
 }
 
-// register();
 // Register
 function register()
 {
@@ -30,7 +32,7 @@ function register()
     $query = "SELECT * FROM UserInfo WHERE email='$useremail'";
     $result = pg_query($conn, $query);
     if (pg_num_rows($result) > 0) {
-        echo json_encode(utf8_encode('User already exists!'));
+        echo json_encode(utf8_encode('User already exists! Kindly login.'));
         $txt = "User already exists!";
         fwrite($myfile, $txt);
         exit();
@@ -38,12 +40,29 @@ function register()
         $userpass = md5($userpass);
         $query = "Insert into UserInfo(username,email,userpassword) values('$username','$useremail','$userpass')";
         $result = pg_query($conn, $query);
-        if ($result) echo 'Registration Successful';;
+        if ($result) echo 'Registration Successful';
     }
 }
 
 function login(){
-    echo 'login called';
+    global $myfile,$conn;
+    // fwrite($myfile,'logging user');
+    $useremail = $_POST['email'];
+    $userpass = md5($_POST['password']);
+    $query = "Select * from UserInfo where email='$useremail' and userpassword='$userpass'";
+    fwrite($myfile,$query);
+    $result=pg_query($conn,$query);
+    if(pg_num_rows($result)==1){
+        $row=pg_fetch_row($result);
+        $loggedUserName=$row[1];
+        $loggedUserEmail=$row[2];
+        $loggedUserPassword=$row[3];
+        $_SESSION['loggedUserName']=$loggedUserName;
+        $_SESSION['loggedUserEmail']=$loggedUserEmail;
+        $_SESSION['loggedUserPassword']=$loggedUserPassword;
+        echo 'Login Successfull '. $loggedUserName;
+    }
+    else echo 'Password Incorrect';
 }
 
 ?>
