0c514dbc6c9a0661f8c4654969c3872cb3597b51
CoderLovely08 <lovelysingeras297@gmail.com>
2022-09-11 15:05:19 +0530

    removed redundant functions

--- Task1/script.php
+++ Task1/script.php
@@ -18,10 +18,10 @@
         }
     }
 
-    function validateLogin(){
+    function validateLogin() {
         if ($('#userpass').val().length < 8) {
             $('#userpass').css('border-color', 'red').after("<br><span>Passwords doesn't match</span>");
-        } else if($('#useremail').val().length==0){
+        } else if ($('#useremail').val().length == 0) {
             $('#useremail').css('border-color', 'red').after("<br><span>Email can't be empty!</span>");
         } else loginUser();
     }
@@ -41,12 +41,12 @@
             success: function(response) {
                 alert(response);
                 console.log("response");
-                if(response==='Registration Successful') window.location = "login.php"
+                if (response === 'Registration Successful') window.location = "login.php"
             }
         });
     }
 
-    function loginUser(){
+    function loginUser() {
         var data = {
             'email': $('#useremail').val(),
             'password': $('#userpass').val(),
@@ -57,16 +57,42 @@
             url: 'process.php',
             type: 'post',
             data: data,
-            success: function(response){
+            success: function(response) {
                 alert(response);
-                if(response==='Login Successfull') window.location='dashboard.php';
+                if (response === 'Login Successfull') window.location = 'dashboard.php';
             }
         });
     }
 
-    function logoutUser(){
+    function logoutUser() {
         session_unset();
         session_destroy();
-        window.location='login.php'
+        window.location = 'login.php'
+    }
+
+    function uploadImage() {
+        var formData = $('#file').prop('files')[0];
+        var form_data = new FormData();
+        form_data.append('file', formData);
+        console.log(form_data);
+        // alert(form_data);
+        var data = {
+            'imageTitle': $('#imageTitle').val(),
+            'imageDescription': $('#imageDescription').val(),
+            'formdata': form_data,
+            'action': 'upload'
+        };
+        $myfile = fopen("logfile.txt", "a");
+        fwrite($myfile, 'upload Image \n');
+        alert('hi');
+        $.ajax({
+            url: 'process.php',
+            type: 'post',
+            data: data,
+            success: function(response) {
+                alert(response);
+                // if(response==='Upload Successfull') window.location='dashboard.php';
+            }
+        });
     }
 </script>
