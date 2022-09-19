38a94f7a72ec499a19452746697f33d134dc2caf
CoderLovely08 <lovelysingeras297@gmail.com>
2022-09-19 21:47:29 +0530

    fixed password hide/show icon bug

--- Task1/index.php
+++ Task1/index.php
@@ -14,20 +14,13 @@ if (isset($_SESSION['loggedUserName'])) header("location: dashboard.php");
     <title>Join the community</title>
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 
     <!-- Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
     <link rel="stylesheet" href="style.css">
     <script>
@@ -53,13 +46,13 @@ if (isset($_SESSION['loggedUserName'])) header("location: dashboard.php");
                     <form autocomplete="off" action="" method="post">
                         <input id="username" name="username" type="text" placeholder="Enter Name" required>
                         <input id="useremail" name="useremail" type="email" placeholder="Enter Email" required>
-                        <p>
-                            <input id="newPassword" name="userpassword" type="password" placeholder="New Password"
-                                required>
+                        
+                        <div class="wrapper">
+                            <input id="newPassword" name="userpass" type="password" placeholder="Enter Password">
                             <i class="bi bi-eye-slash" id="togglePassword"></i>
-                            <input id="confirmPassword" name="confirmpassword" type="password"
-                            placeholder="Confirm Password" required>
-                        </p>
+                        </div>
+                        <input id="confirmPassword" name="confirmpassword" type="password" placeholder="Confirm Password" required>
+                        
                         <a href="" id="submit" onclick="validatePassword();">Signup</a>
                     </form>
                     <h6>Already have an account? <a href="login.php">Login</a></h6>
